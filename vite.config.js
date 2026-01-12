ac06c7fbfc0f0852e61a7b9ed52eb9e4ae916574
Ivan Pavlovic <ipdeveloper2001@gmail.com>
2026-01-12 13:34:33 +0100

    Configure base path and build options in Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reset-qr/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    emptyOutDir: true
+  }
 })
